151d577c451bfa2d66ca4d64f627d979d038024f
Franklin20116 <fbcarreno@alumno.montepiedra.edu.ec>
2025-10-06 13:16:54 -0500

    Create Año bisiesto.cpp
    Se trata de validar es un año bisiesto

---
+++ Año bisiesto.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main (){
+  int anio;
+  cou<< "Ingrese un año:";
+  cin>> anio;
+
+  if((anio % 4 == 0&& anio % 100 !=0)||(anio %400==0)
+    cout<< "El año es bisiesto." << endl;
+esle
+    cout<< "El año no es bisiesto." << endl;
+
+return 0;
+}
