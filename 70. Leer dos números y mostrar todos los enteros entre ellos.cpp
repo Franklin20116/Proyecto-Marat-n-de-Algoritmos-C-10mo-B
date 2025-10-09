c8f7b65be506ebb58f77fbf33149bbfa50fe3f80
Franklin20116 <fbcarreno@alumno.montepiedra.edu.ec>
2025-10-09 11:33:20 -0500

    70. Leer dos números y mostrar todos los enteros entre ellos.cpp
    Muestra rango de enteros.

---
+++ 70. Leer dos números y mostrar todos los enteros entre ellos.cpp
@@ -0,0 +1,11 @@
+#include 
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    for (int i = min(a, b); i <= max(a, b); i++) {
+        cout << i << " ";
+    }
+    return 0;
+}
