e7510c1136c5b1ce976502779ae075263544660f
Franklin20116 <fbcarreno@alumno.montepiedra.edu.ec>
2025-10-12 15:36:25 -0500

    4. Mostrar nombre y edad.cpp

---
+++ 4. Mostrar nombre y edad.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+uing namespace std;
+s
+int main() {
+    string nombre;
+    int edad;
+    cin >> nombre >> edad;
+    cout << "Nombre: " << nombre << ", Edad: " << edad;
+    return 0;
+}
