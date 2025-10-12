b7e5fe7259fd22b4f1a15f5d2d9e83cc16c18a78
Franklin20116 <fbcarreno@alumno.montepiedra.edu.ec>
2025-10-12 17:35:44 -0500

    149. Verificar si número tiene al menos un dígito igual a 9.cpp
    Busca el dígito 9.

---
+++ 149. Verificar si número tiene al menos un dígito igual a 9.cpp
@@ -0,0 +1,17 @@
+#include 
+using namespace std;
+
+int main() {
+    int n;
+    bool contieneNueve = false;
+    cin >> n;
+    while (n != 0) {
+        if (n % 10 == 9) {
+            contieneNueve = true;
+            break;
+        }
+        n /= 10;
+    }
+    cout << (contieneNueve ? "Contiene 9" : "No contiene 9");
+    return 0;
+}
