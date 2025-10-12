7c3de1a463d05dbf8c94a6296e7c0609fb5b8fba
Franklin20116 <fbcarreno@alumno.montepiedra.edu.ec>
2025-10-12 16:17:24 -0500

    20. Mostrar inicial y apellido.cpp

---
+++ 20. Mostrar inicial y apellido.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char inicial;
+    string apellido;
+    cin >> inicial >> apellido;
+    cout << inicial << ". " << apellido;
+    return 0;
+}
