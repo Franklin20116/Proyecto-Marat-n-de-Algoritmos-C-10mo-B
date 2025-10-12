78f715b15aed43e2af7f9399c0ceaf30806f19a8
Franklin20116 <fbcarreno@alumno.montepiedra.edu.ec>
2025-10-12 15:53:57 -0500

    10. Potencia de un número.cpp

---
+++ 10. Potencia de un número.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    double base, exponente;
+    cin >> base >> exponente;
+    cout << pow(base, exponente);
+    return 0;
+}
