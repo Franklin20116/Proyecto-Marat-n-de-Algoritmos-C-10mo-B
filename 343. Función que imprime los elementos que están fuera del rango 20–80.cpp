df3229c00541c5033e01f6cf67c0bf0bf129dd38
Franklin20116 <fbcarreno@alumno.montepiedra.edu.ec>
2025-10-13 16:38:34 -0500

    343. Función que imprime los elementos que están fuera del rango 20–80.cpp
     Excluye rango 20–80.

---
+++ 343. Función que imprime los elementos que están fuera del rango 20–80.cpp
@@ -0,0 +1,8 @@
+#include 
+using namespace std;
+
+void imprimirFueraDe20y80(int arr[], int n) {
+    for (int i = 0; i < n; i++)
+        if (arr[i] < 20 || arr[i] > 80)
+            cout << arr[i] << " ";
+}
