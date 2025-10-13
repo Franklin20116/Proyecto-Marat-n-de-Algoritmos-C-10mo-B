d5e83c0bbd9856589ff3c5afdb1da873256aae69
Franklin20116 <fbcarreno@alumno.montepiedra.edu.ec>
2025-10-13 16:22:40 -0500

    327. Función que imprime los elementos que son múltiplos de 5 y no de 10.cpp
    Múltiplos de 5 excluyendo 10.

---
+++ 327. Función que imprime los elementos que son múltiplos de 5 y no de 10.cpp
@@ -0,0 +1,8 @@
+#include 
+using namespace std;
+
+void imprimirMultiplos5No10(int arr[], int n) {
+    for (int i = 0; i < n; i++)
+        if (arr[i] % 5 == 0 && arr[i] % 10 != 0)
+            cout << arr[i] << " ";
+}
