7c29d7b8fac188111a7328d372f35159fd1b8546
Franklin20116 <fbcarreno@alumno.montepiedra.edu.ec>
2025-10-20 17:40:12 -0500

    Ejercicio 30 – Contar letras mayúsculas.cpp

---
+++ Ejercicio 30 – Contar letras mayúsculas.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int contarMayusculas(char* cad) {
+    int contador = 0;
+    while (*cad) {
+        if (*cad >= 'A' && *cad <= 'Z') contador++;
+        cad++;
+    }
+    return contador;
+}
+
+int main() {
+    char frase[200];
+    cout << "Ingrese una frase: ";
+    cin.getline(frase, 200);
+    cout << "Mayúsculas encontradas: " << contarMayusculas(frase) << endl;
+    return 0;
+}
+
